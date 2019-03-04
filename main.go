f9765f33556698541415ef999896934d658aa452
Rizbe <mohammedh@zillowgroup.com>
2019-03-03 21:43:10 -0500

    re structure

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/Rizbe/terraform-provider-npm/npm"
 )
 
 func main() {
 
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return npm.Provider()
 		},
 	})
 
--- npm/provider.go
+++ npm/provider.go
@@ -1,4 +1,4 @@
-package main
+package npm
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
